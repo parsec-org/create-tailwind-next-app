436c2ed862e3e5f668aa2f89c2b121687cd42496
hcyang <hqwlkj@outlook.com>
2024-08-09 16:56:19 +0800

    feat: add nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,34 @@
 import '@/styles/globals.css';
+import 'nprogress/nprogress.css';
+import { useEffect } from 'react';
 import { NextIntlClientProvider } from 'next-intl';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
+import NProgress from 'nprogress';
+
+NProgress.configure({ showSpinner: false });
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
+  useEffect(() => {
+    const handleStart = () => {
+      NProgress.start();
+    };
+
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
   return (
     <NextIntlClientProvider
       // To achieve consistent date, time and number formatting
