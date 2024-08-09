ac71e9eefe74f5df98b03b6b3bbdeb9cb6497069
hcyang <hqwlkj@outlook.com>
2024-08-09 17:11:08 +0800

    fix: eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,9 +40,11 @@ export default function Home() {
         />
         <div>{t('title')}</div>
         <div>{t('description')}</div>
+        <div>
+          ewqeeeeqeqeqeweqewqeqwewq
+        </div>
         <Link href={'/about'}>{t('nav.about')}</Link>
       </div>
-
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href={t('items.doc.link')}
@@ -58,7 +60,6 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>{t('items.doc.description')}</p>
         </a>
-
         <a
           href={t('items.learn.link')}
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
