51a098c8b50dec28295e5a50cbe3aa7026c10e4a
hcyang <hqwlkj@outlook.com>
2024-08-09 17:15:15 +0800

    fix: test eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ['latin'] });
 export default function Home() {
   const t = useTranslations('Index');
 
-  const ddss= dddd;
+  const ddsscccc = dddd;
 
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
