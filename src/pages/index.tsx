752334b46b85d99ba7e8b003c48d1383e118a089
hcyang <hqwlkj@outlook.com>
2024-08-09 17:20:06 +0800

    fix: test eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ export default function Home() {
 
   const ddsscccc = dddd;
   const ddsscccc = dddd;
+  const ddsscccc = dddd;
 
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
