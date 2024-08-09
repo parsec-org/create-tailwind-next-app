2eb22e246e5a08e98cbb3349d692b96c2b1cebe0
hcyang <hqwlkj@outlook.com>
2024-08-09 17:12:35 +0800

    fix: eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   const t = useTranslations('Index');
+  const adddd = eeee;
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
