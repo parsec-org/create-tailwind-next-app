74b2527609a070493669d5b0b80c14d7537739c6
hcyang <hqwlkj@outlook.com>
2024-08-12 17:35:41 +0800

    fix: test eslint

--- src/components/Commons/LocaleSwitcher/index.tsx
+++ src/components/Commons/LocaleSwitcher/index.tsx
@@ -1,4 +1,4 @@
-import { useTranslations, useLocale } from 'next-intl';
+import { useTranslations } from 'next-intl';
 import { ChangeEvent, useCallback, useTransition } from 'react';
 import { useParams, usePathname } from 'next/navigation';
 import { useRouter } from 'next/router';
--- src/types/typings.d.ts
+++ src/types/typings.d.ts
@@ -4,5 +4,5 @@ type Messages = typeof en;
 
 declare global {
   // Use type safe message keys with `next-intl`
-  interface IntlMessages extends Messages {}
+  type IntlMessages = Messages;
 }
