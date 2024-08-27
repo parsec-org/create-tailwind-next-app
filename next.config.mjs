9d9ded1fad34224ffc555124ba52a4d0ff511be9
hcyang <hqwlkj@outlook.com>
2024-08-27 17:01:55 +0800

    chore: lint

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   i18n: {
-    locales: ["en", "de", "zh"],
-    defaultLocale: "en",
+    locales: ['en', 'de', 'zh'],
+    defaultLocale: 'en',
   },
 };
 
